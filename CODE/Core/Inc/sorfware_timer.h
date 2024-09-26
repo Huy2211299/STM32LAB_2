ad954f86515af4287ade281e7b03b272bacf58a5
vohoanghuy <vohoanghuy7705@gmail.com>
2024-09-26 15:59:33 +0700

    init for Ex1

--- CODE/Core/Inc/sorfware_timer.h
+++ CODE/Core/Inc/sorfware_timer.h
@@ -8,9 +8,14 @@
 #ifndef INC_SORFWARE_TIMER_H_
 #define INC_SORFWARE_TIMER_H_
 
+#include "main.h"
 extern int timer1_flag;
 
+#define LED_1 1
+#define LED_2 2
+
 void setTimer1(int duration);
+void chay();
 void timerRun();
 
 #endif /* INC_SORFWARE_TIMER_H_ */
--- CODE/Core/Src/sorfware_timer.c
+++ CODE/Core/Src/sorfware_timer.c
@@ -9,10 +9,54 @@
 int timer1_counter = 0;
 int timer1_flag = 0;
 
+int led7_status = LED_1;
+
 void setTimer1(int duration){
 	timer1_counter = duration;
 	timer1_flag = 0;
+}
 
+void display7SEG(int num){
+	switch(num){
+		case 1:
+			HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(SEG3_GPIO_Port, SEG3_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(SEG5_GPIO_Port, SEG5_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, GPIO_PIN_SET);
+			break;
+		case 2:
+			HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(SEG3_GPIO_Port, SEG3_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(SEG5_GPIO_Port, SEG5_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, GPIO_PIN_RESET);
+			break;
+		default:
+			break;
+	}
+}
+void chay(){
+	switch(led7_status){
+		case 1:
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+			display7SEG(1);
+			led7_status = 2;
+			break;
+		case 2:
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
+			display7SEG(1);
+			led7_status = 2;
+			break;
+		default:
+			break;
+	}
 }
 void timerRun(){
 	if(timer1_counter > 0){
@@ -22,4 +66,3 @@ void timerRun(){
 		}
 	}
 }
-
