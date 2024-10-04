645e2a897d344814e6435e903fd24f38186b85f6
vohoanghuy <vohoanghuy7705@gmail.com>
2024-10-04 07:39:49 +0700

    init for Ex8

--- CODE/Core/Src/sorfware_timer.c
+++ CODE/Core/Src/sorfware_timer.c
@@ -158,7 +158,7 @@ void updateClockBuffer(){
 	led_buffer[0] = hour / 10;
 	led_buffer[1] = hour % 10;
 	led_buffer[2] = minute / 10;
-	led_buffer[3] = minute % 10;
+	led_buffer[3] = minute % 10; //
 }
 int timer1_flag = 0;
 int timer1_counter = 0;
