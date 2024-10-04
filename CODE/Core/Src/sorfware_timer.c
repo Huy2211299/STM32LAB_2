eef0737612e7db017f3f36246d640c1c6f4c1015
vohoanghuy <vohoanghuy7705@gmail.com>
2024-10-04 07:53:39 +0700

    init for Ex9

--- CODE/Core/Src/sorfware_timer.c
+++ CODE/Core/Src/sorfware_timer.c
@@ -155,22 +155,10 @@ void update7SEG ( int index ){
 int index_led = 0;
 int led_buffer [4] = {1 , 2 , 3 , 4};
 void updateClockBuffer(){
-	if(hour <= 9){
-		led_buffer[0] = 0;
-		led_buffer[1] = hour;
-	}
-	if(hour >= 10){
-		led_buffer[0] = hour / 10;
-		led_buffer[1] = hour % 10;
-	}
-	if(minute <= 9){
-		led_buffer[2] = 0;
-		led_buffer[3] = minute;
-	}
-	if(minute >= 10){
-		led_buffer[2] = minute / 10;
-		led_buffer[3] = minute % 10;
-	}
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute / 10;
+	led_buffer[3] = minute % 10;
 }
 int index_led_matrix = 0;
 uint8_t matrix_buffer [8] = {0x18,0x3c,0x66,0x66,0x7e,0x7e,0x66,0x66};
